07b5f78643b9d519ab29cd6683078fa4147cd5a4
Ayush Singh <ayush5292singh@gmail.com>
2026-07-29 22:20:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,90 @@
-const search=document.getElementById("search");
+let currentChat="";
 
-search.addEventListener("keyup",function(){
 
-let value=search.value.toLowerCase();
+function openChat(name){
 
-let chats=document.querySelectorAll(".chat");
+currentChat=name;
 
-chats.forEach(chat=>{
+document.getElementById("home").classList.add("hidden");
 
-let name=chat.querySelector("h3").textContent.toLowerCase();
+document.getElementById("chatScreen").classList.remove("hidden");
 
-if(name.includes(value)){
-chat.style.display="flex";
+document.getElementById("chatName").innerHTML=name;
+
+loadMessages();
+
 }
-else{
-chat.style.display="none";
+
+
+
+function goBack(){
+
+document.getElementById("chatScreen").classList.add("hidden");
+
+document.getElementById("home").classList.remove("hidden");
+
 }
+
+
+
+function sendMessage(){
+
+let input=document.getElementById("messageInput");
+
+let text=input.value.trim();
+
+
+if(text==="") return;
+
+
+let messages=JSON.parse(
+localStorage.getItem(currentChat)
+) || [];
+
+
+messages.push(text);
+
+
+localStorage.setItem(
+currentChat,
+JSON.stringify(messages)
+);
+
+
+input.value="";
+
+loadMessages();
+
+}
+
+
+
+function loadMessages(){
+
+let box=document.getElementById("messages");
+
+box.innerHTML="";
+
+
+let messages=JSON.parse(
+localStorage.getItem(currentChat)
+) || [];
+
+
+messages.forEach(msg=>{
+
+box.innerHTML+=
+`
+<div class="message">
+${msg}
+<br>
+<small>${new Date().toLocaleTimeString()}</small>
+</div>
+`;
 
 });
 
-});
+
+box.scrollTop=box.scrollHeight;
+
+}
